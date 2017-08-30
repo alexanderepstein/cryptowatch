63470ad3553a0ef5e67d0cc5d76e16c735ee2ff2
Alex Epstein <epsteina@wit.edu>
2017-08-30 04:38:28 -0400

    General fixes and features
    Added file option.
    Caught keyboard interrupt.
    Print out to the console format is different.
    Fixing bugs with lack of two imports.
    Added last two fields to console output.
    Clean exit.

--- cryptoConsole/cryptowatch.py
+++ cryptoConsole/cryptowatch.py
@@ -23,6 +23,9 @@
 # SOFTWARE.
 import argparse
 import time
+from sys import platform
+from os import system
+
 
 import utils.cryptoUtils as crypto
 import utils.cwconfig as cfg
@@ -31,18 +34,12 @@ config = cfg.config()
 
 
 
-if platform == "linux" or platform == "linux2" or platform == "darwin":
-    from os.path import expanduser
-    home = expanduser("~")
-    monitorFilePath = home + '/.cryptoConsole'
-else:
-    monitorFilePath = 'C:/.cryptoConsole'
-
 def clear():
     if platform == "linux" or platform == "linux2" or platform == "darwin":
         system("clear")
     elif platform == "win32":
         system("cls")
+
 
 def printHeader():
     print('_________                        __                         __         .__')
@@ -50,9 +47,21 @@ def printHeader():
     print('/    \  \/\_  __ <   |  |\____ \   __\/  _ \ \/ \/ /\__  \\\\   __\/ ___\|  |  \\')
     print('\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  | \  \___|   Y  \\')
     print('\______  /|__|   / ____||   __/|__|   \____/ \/\_/  (____  /__|  \___  >___|  /')
-    print('       \/        \/     |__|                            \/          \/     \/')
+    print('       \/        \/     |__|                             \/          \/     \/')
 
-def consoleMonitor(coinType):
+
+"""
+Output: Appends the information about a certain cryptocurrency to a file
+Parameter coinType: Specifcies which coin to write the information about
+Logic:
+    - Get the respective address array for the coinType
+    - Query coinmarketcap about the coinType
+    - Parse the response
+    - Get total amount fiat amount
+    - Determine total crypto amount
+    - Format and write the data to the monitorFilePath
+"""
+def consoleMonitor(coinType, monitorFilePath):
     coinType = coinType.lower()
     if coinType == "ethereum":
         cryptoTicker = "ETH"
@@ -69,29 +78,54 @@ def consoleMonitor(coinType):
     exchangeRate = float(crypto.parseCryptoData(response, "ER"))
     hourlyPercentage = float(crypto.parseCryptoData(response, "HP"))
     dailyPercentage = float(crypto.parseCryptoData(response, "DP"))
+    weeklyPercentage = float(crypto.parseCryptoData(response, "WP"))
+    dailyVolume = float(crypto.parseCryptoData(response, "DV"))
     totalFiat = float(crypto.getTotalFiat(crypto.parseCryptoData(response, "ER"), coinType))
     totalCrypto = float(totalFiat) / float(exchangeRate)
     with open(monitorFilePath, 'a+') as file:
         file.write("%s->%s:%.2f\n" % (cryptoTicker, config.fiatCurrency, exchangeRate))
-        file.write("1H: %.2f  24H: %.2f\n" % (hourlyPercentage, dailyPercentage))
+        file.write("1H: %.2f%%  24H: %.2f%%\n" % (hourlyPercentage, dailyPercentage))
+        file.write("7 day: %.2f%%   24H Volume: %.2f\n" % (weeklyPercentage, dailyVolume))
         if totalFiat != 0 and address is not None :
-            file.write("%s: %.2f\n" % (cryptoTicker, totalCrypto))
-            file.write("%s: %.2f\n" % (config.fiatCurrency, totalFiat))
+            file.write("%s: %.2f   %s: %.2f\n" % (cryptoTicker, totalCrypto, config.fiatCurrency, totalFiat))
         file.write("\n")
 
 def main():
-    parser = argparse.ArgumentParser(prog="Cryptowatch",description='Track prices and account balances for bitcoin, ethereum, and litecoin')
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        from os.path import expanduser
+        home = expanduser("~")
+        monitorFilePath = home + '/.cryptoConsole'
+    else:
+        monitorFilePath = 'C:/.cryptoConsole'
+    parser = argparse.ArgumentParser(prog="Cryptowatch",description='Track prices and account balances for bitcoin, ethereum, and litecoin', epilog="By: Alex Epstein https://github.com/alexanderepstein")
     parser.add_argument("-m", "--monitor",help="Choose which cryptowatch monitor to use")
-    parser.add_argument("-v", "--version", action="store_true", help="Display the current version of cryptowatch")
+    parser.add_argument("-f", "--file", default="", help="Output the current cryptowatch data to the specified file path")
     parser.add_argument("-c", "--config", action = "store_true", help="Edit the config file for cryptowatch")
+    parser.add_argument("-v", "--version", action="store_true", help="Display the current version of cryptowatch")
     args = parser.parse_args()
     if args.version:
         print("Cryptowatch Version 0.0.3")
-        exit()
-    if args.config:
+    elif args.config:
         config.edit()
-        exit()
-    if args.monitor:
+    elif args.file != "":
+        filePath = args.file
+        if platform == "linux" or platform == "linux2" or platform == "darwin":
+            if "~" in filePath:
+                from re import sub
+                from os.path import expanduser
+                filePath = sub("~",expanduser("~"),filePath)
+        try:
+            open(filePath, 'w+').close()
+        except IsADirectoryError:
+            print("Error: the path provided is a directory")
+            exit()
+        print("Loading...")
+        consoleMonitor("ethereum", filePath)
+        consoleMonitor("bitcoin",filePath)
+        consoleMonitor("litecoin", filePath)
+        clear()
+        printHeader()
+    elif args.monitor:
         if args.monitor == "pie" or args.monitor == "rpi":
             import cryptoPie.cryptoPie as pie
             printHeader()
@@ -99,28 +133,35 @@ def main():
         elif args.monitor == "web":
             printHeader()
             print("Web monitor not implemented yet.")
-        elif args.monitor == "console":
+        elif args.monitor == "console" or args.monitor == "terminal":
+            from datetime import datetime
             print("Loading...")
             open(monitorFilePath, 'w+').close()
-            while True:
-                consoleMonitor("ethereum")
-                consoleMonitor("bitcoin")
-                consoleMonitor("litecoin")
+            try:
+                while True:
+                    consoleMonitor("ethereum", monitorFilePath)
+                    consoleMonitor("bitcoin", monitorFilePath)
+                    consoleMonitor("litecoin", monitorFilePath)
+                    clear()
+                    printHeader()
+                    with open(monitorFilePath, 'r') as file:
+                        print(file.read())
+                    print("Last Updated: %s" % datetime.now().strftime('%H:%M:%S'))
+                    print("Watching...")
+                    time.sleep(2)
+                    open(monitorFilePath, 'w').close()
+            except KeyboardInterrupt:
+                open(monitorFilePath, 'w').close()
                 clear()
                 printHeader()
-                with open(monitorFilePath, 'r') as file:
-                    print(file.read())
-                print("Watching...")
-                time.sleep(2)
-                open(monitorFilePath, 'w').close()
         else:
             print("Error: invalid monitor type")
     else:
             print("Loading...")
             open(monitorFilePath, 'w+').close()
-            consoleMonitor("ethereum")
-            consoleMonitor("bitcoin")
-            consoleMonitor("litecoin")
+            consoleMonitor("ethereum", monitorFilePath)
+            consoleMonitor("bitcoin", monitorFilePath)
+            consoleMonitor("litecoin", monitorFilePath)
             clear()
             printHeader()
             with open(monitorFilePath, 'r') as file:
