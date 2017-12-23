9b1e5b1b7fb59387569145320e5cc3c4e5760f34
delirious-lettuce <delirious.lettuce@gmail.com>
2017-12-22 20:32:18 -0700

    `cryptowatch` cleanup:   * Format lines longer than 79 characters   * Group imports according to PEP8 (standard, local, third-party)   * Merge the two `os.path` imports to a single line   * Only create `config` variable if needed   * Make `HEADER` a constant (also removes line length issues)   * Use `snake_case` for function and variable names (PEP8)   * Use `in` instead of multiple consecutive `==` or `!=` comparisons   * Simplify subsequent args checks by using a mutually exclusive group   * Remove `default=''` from `file` argument (default is already None)   * Remove excess whitespace around `=` operator in `config` argument   * Use `choices` parameter for `monitor` argument

--- cryptoConsole/cryptowatch.py
+++ cryptoConsole/cryptowatch.py
@@ -11,8 +11,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -21,84 +21,105 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 import argparse
-
-from time import sleep
-from os.path import exists
-from sys import platform
+from os.path import exists, expanduser
 from re import sub
-from os.path import expanduser
+from sys import platform
+from time import sleep
 
-
+import cryptoUtils.cwconfig as cfg
 from cryptoUtils.cryptoUtils import clear
 from cryptoUtils.cryptoUtils import getCryptoTable
-import cryptoUtils.cwconfig as cfg
+
+HEADER = r'''
+_________                        __                         __         .__
+\_   ___ \_______ ___.__._______/  |_  ______  _  _______ _/  |_  ____ |  |__
+/    \  \/\_  __ <   |  |\____ \   __\/  _ \ \/ \/ /\__  \\   __\/ ___\|  |  \
+\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  | \  \___|   Y  \
+\______  /|__|   / ____||   __/|__|   \____/ \/\_/  (____  /__|  \___  >___|  /
+       \/        \/     |__|                             \/          \/     \/
+         Created by: Alex Epstein https://github.com/alexanderepstein'''
 
 
-config = cfg.config()
+def print_header():
+    print(HEADER)
 
-def printHeader():
-    print('_________                        __                         __         .__')
-    print('\_   ___ \_______ ___.__._______/  |_  ______  _  _______ _/  |_  ____ |  |__')
-    print('/    \  \/\_  __ <   |  |\____ \   __\/  _ \ \/ \/ /\__  \\\\   __\/ ___\|  |  \\')
-    print('\     \____|  | \/\___  ||  |_> >  | (  <_> )     /  / __ \|  | \  \___|   Y  \\')
-    print('\______  /|__|   / ____||   __/|__|   \____/ \/\_/  (____  /__|  \___  >___|  /')
-    print('       \/        \/     |__|                             \/          \/     \/')
-    print("         Created by: Alex Epstein https://github.com/alexanderepstein")
 
-def cryptoFile(filePath):
-    if platform in ["linux", "linux2", "darwin"]:
-        if "~" in filePath:
-            filePath = sub("~", expanduser("~"), filePath)
-    if exists(filePath):
-        answer = input("File already exists at %s, overwrite it? [Y/n] " % filePath)
-        answer = answer.lower()
-        if answer != 'y' and answer != "yes":
+def crypto_file(file_path):
+    if platform in ("linux", "linux2", "darwin") and "~" in file_path:
+        file_path = sub("~", expanduser("~"), file_path)
+    if exists(file_path):
+        answer = input("File already exists at %s, overwrite it? [Y/n] "
+                       % file_path)
+        if answer.lower().strip() not in ('y', 'yes'):
             exit()
     try:
-        with open(filePath, 'w+') as file:
+        with open(file_path, 'w+') as file:
             data = getCryptoTable(False, False)
             file.write(data)
             file.write("\n")
     except IsADirectoryError:
         print("Error: the path provided is a directory")
         exit()
-    printHeader()
+    print_header()
 
 
-def consoleLoop():
+def console_loop():
     try:
         while True:
             print(getCryptoTable(True))
             sleep(30)
     except KeyboardInterrupt:
         clear()
-        printHeader()
+        print_header()
+
 
 def main():
-    parser = argparse.ArgumentParser(prog="Cryptowatch",description='Track prices and account balances for bitcoin, ethereum, and litecoin', epilog="By: Alex Epstein https://github.com/alexanderepstein")
-    parser.add_argument("-m", "--monitor",help="Choose which cryptowatch monitor to use")
-    parser.add_argument("-f", "--file", default="", help="Output the current cryptowatch data to the specified file path")
-    parser.add_argument("-c", "--config", action = "store_true", help="Edit the config file for cryptowatch")
-    parser.add_argument("-v", "--version", action="store_true", help="Display the current version of cryptowatch")
+    parser = argparse.ArgumentParser(
+        prog="Cryptowatch",
+        description='Track prices and account balances for bitcoin, '
+                    'ethereum, and litecoin',
+        epilog="By: Alex Epstein https://github.com/alexanderepstein"
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-m",
+        "--monitor",
+        choices=('pie', 'rpi', 'console', 'terminal'),
+        help="Choose which cryptowatch monitor to use"
+    )
+    group.add_argument(
+        "-f",
+        "--file",
+        help="Output the current cryptowatch data to the specified file path"
+    )
+    group.add_argument(
+        "-c",
+        "--config",
+        action="store_true",
+        help="Edit the config file for cryptowatch"
+    )
+    group.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        help="Display the current version of cryptowatch"
+    )
     args = parser.parse_args()
-    if (args.version or args.config or args.file or args.monitor) and not (bool(args.version) ^ bool(args.config) ^ bool(args.monitor) ^ bool(args.file != "")):
-        print("Error: all options for cryptowatch are mutually exclusive")
-        exit()
+
     if args.version:
         print("Cryptowatch Version 0.0.7")
     elif args.config:
-        config.edit()
-    elif args.file != "":
-        cryptoFile(args.file)
+        cfg.config().edit()
+    elif args.file:
+        crypto_file(args.file)
     elif args.monitor:
-        if args.monitor == "pie" or args.monitor == "rpi":
+        if args.monitor in ("pie", "rpi"):
             import cryptoPie.cryptoPie as pie
-            printHeader()
+            print_header()
             pie.main()
-        elif args.monitor == "console" or args.monitor == "terminal" or args.monitor == "":
-            consoleLoop()
         else:
-            print("Error: invalid monitor type")
+            console_loop()
     else:
         print(getCryptoTable())
